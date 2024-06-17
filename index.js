ef08bf862bee51d2a1c3a78f27ec10bea38d5b4b
Henry <cyrus.usa@gmail.com>
2024-06-17 15:57:58 +0200

    parsing finished

--- index.js
+++ index.js
@@ -15,21 +15,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/week", async (req, res) => {
-  res.send("API running.");
-
   const url = `https://www.thaiorchid-karlsruhe.de/wochenkarte.html`;
   try {
     const result = await axios.get(url);
     const data = result.data;
-    //console.log(data);
+
     const $ = cheerio.load(data);
 
-    //TODO: day is not exact yet, needs rework
-    // const $day = $("span.fs14lh1-5:contains(':')").text();
-    //console.log($day);
-
-    //const dish = $("span.fs14lh1-5.ff2:contains(',')").text().trim();
-    // console.log(dish);
     let reg = new RegExp("[a-zA-Z:]");
     function filterABC() {
       return reg.test($(this).text());
@@ -41,28 +33,81 @@ app.get("/week", async (req, res) => {
       .filter(filterABC)
       .each(function () {
         const food = $(this).text().trim();
-        dishes.push({
-          food,
-        });
+        dishes.push(food);
       });
     //console.log(dishes);
 
     //remove not needed information
     const modDishes = dishes.slice(4);
-    // console.log(modDishes);
+    //onsole.log(modDishes);
 
     //find indexOf element
 
     const targetFood = "Alle Gerichte auch zum Mitnehmen !!!";
-    const index = modDishes.findIndex((food) => food.food === targetFood);
+    const index = modDishes.findIndex((food) => food === targetFood);
 
     if (index !== -1) {
-      console.log(`Found ${targetFood} at index ${index}`);
+      // console.log(`Found ${targetFood} at index ${index}`);
     } else {
       console.log(`${targetFood} not found`);
     }
     const modDishes2 = modDishes.slice(0, index);
-    console.log(modDishes2);
+    //console.log(modDishes2);
+
+    const regDay = new RegExp("(Mittwoch)|(tag)");
+    const regFood = new RegExp("(Paprika)|(verschiedenem)|(Basilikum)");
+    const regRice = new RegExp("(Reis)");
+
+    let weekDish = [];
+
+    //merge elements of days to previous line, if regexes match
+    for (let index = 0; index < modDishes2.length; index++) {
+      if (regDay.test(modDishes2[index])) {
+        weekDish[index] = modDishes2[index];
+      } else if (regRice.test(modDishes2[index])) {
+        weekDish[index - 1] += " " + modDishes2[index];
+      } else {
+        weekDish[index] = modDishes2[index];
+      }
+    }
+
+    //removing empty elements:
+    let weekDish2 = [];
+
+    parseFood(weekDish, weekDish2);
+    let weekDish3 = [];
+    parseFood(weekDish2, weekDish3);
+    //console.log(weekDish3);
+    let weekDish4 = [];
+    parseFood(weekDish3, weekDish4);
+
+    const filteredArray = weekDish4.filter(
+      (element) => element != null || element != undefined
+    );
+    filteredArray[0] = filteredArray[0].replace(/\s+/g, " ");
+
+    //console.log(filteredArray);
+    const regWeekDays =
+      /(Montag:)|(Dienstag:)|(Mittwoch:)|(Donnerstag:)|(Freitag:)/gi;
+    const trimmedArray = filteredArray.map((element) => {
+      // Replace 'tag' or 'Mittwoch' with a space
+      return element.replace(regWeekDays, "").trim();
+    });
+
+    // console.log(trimmedArray);
+
+    function parseFood(foodArray, emptyArray) {
+      for (let index = 0; index < foodArray.length; index++) {
+        if (regDay.test(foodArray[index])) {
+          emptyArray[index] = foodArray[index];
+        } else if (regRice.test(foodArray[index])) {
+          emptyArray[index - 1] += " " + foodArray[index];
+        } else {
+          emptyArray[index] = foodArray[index];
+        }
+      }
+      return emptyArray;
+    }
 
     //days of week
     const days = [
@@ -91,7 +136,16 @@ app.get("/week", async (req, res) => {
         food[index].week = days[z];
       }
     }
-    //console.log(food);
+
+    const foodWeekOne = food.slice(0, 1);
+
+    Object.keys(foodWeekOne[0].week).forEach((key, index) => {
+      // Assign the corresponding value from the 'trimmedArray' array to each day
+      foodWeekOne[0].week[key] = trimmedArray[index];
+    });
+
+    //console.log(foodWeekOne);
+    res.json(foodWeekOne);
   } catch (error) {
     console.error(error);
   }
